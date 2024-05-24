997d12726ca9667fe1dfe9f88f42585b86574715
yongj.zhuang <yongj.zhuang@outlook.com>
2024-05-24 23:27:13 +0800

    Make KNodeGraphBuilder thread-safe

--- graph/builder.go
+++ graph/builder.go
@@ -1,24 +1,39 @@
 package graph
 
-import "github.com/curtisnewbie/grapher/datastruct"
+import (
+	"sync"
+
+	"github.com/curtisnewbie/grapher/datastruct"
+)
 
 // Builder that manages keys, nodes and ids.
+//
+// This builder is thread-safe.
 type KNodeGraphBuilder struct {
+	mu         sync.RWMutex
 	idCnt      int
 	keyedNodes map[string]Node
 	keyedEdges map[string]datastruct.Set[string]
 }
 
 func (b *KNodeGraphBuilder) BuildDGraph(title string) (*DGraph, error) {
-	return NewDGraph(title, b.Nodes(), b.Edges())
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return NewDGraph(title, b._nodes(), b._edges())
 }
 
-func (b *KNodeGraphBuilder) Find(k string) (Node, bool) {
+func (b *KNodeGraphBuilder) _find(k string) (Node, bool) {
 	v, ok := b.keyedNodes[k]
 	if ok {
 		return v, true
 	}
 	return Node{}, false
+}
+
+func (b *KNodeGraphBuilder) Find(k string) (Node, bool) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return b._find(k)
 }
 
 // Attempt to add node to builder using the given key.
@@ -29,6 +44,8 @@ func (b *KNodeGraphBuilder) Find(k string) (Node, bool) {
 //
 // If the key doesn't exist, the given node is assigned a id and added to the builder.
 func (b *KNodeGraphBuilder) Add(k string, node Node) (Node, bool) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
 	v, ok := b.keyedNodes[k]
 	if ok {
 		return v, false
@@ -40,6 +57,8 @@ func (b *KNodeGraphBuilder) Add(k string, node Node) (Node, bool) {
 }
 
 func (b *KNodeGraphBuilder) Connect(k1 string, k2 string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
 	s, ok := b.keyedEdges[k1]
 	if ok {
 		s.Add(k2)
@@ -51,6 +70,12 @@ func (b *KNodeGraphBuilder) Connect(k1 string, k2 string) {
 }
 
 func (b *KNodeGraphBuilder) Nodes() []Node {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return b._nodes()
+}
+
+func (b *KNodeGraphBuilder) _nodes() []Node {
 	nodes := make([]Node, 0, len(b.keyedNodes))
 	for k := range b.keyedNodes {
 		nodes = append(nodes, b.keyedNodes[k])
@@ -59,15 +84,21 @@ func (b *KNodeGraphBuilder) Nodes() []Node {
 }
 
 func (b *KNodeGraphBuilder) Edges() []DEdge {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return b._edges()
+}
+
+func (b *KNodeGraphBuilder) _edges() []DEdge {
 	edges := make([]DEdge, 0, len(b.keyedEdges))
 	for k, ed := range b.keyedEdges {
-		kn, ok := b.Find(k)
+		kn, ok := b._find(k)
 		if !ok {
 			continue
 		}
 		// k -> nb
 		for _, nb := range ed.CopyKeys() {
-			nn, ok := b.Find(nb)
+			nn, ok := b._find(nb)
 			if !ok {
 				continue
 			}
