548758d4c18f7899a5ed5ab26c2ea878f95d75c6
yongj.zhuang <yongj.zhuang@outlook.com>
2024-05-24 23:33:27 +0800

    More test

--- graph/builder_test.go
+++ graph/builder_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/curtisnewbie/grapher/sys"
@@ -17,6 +18,42 @@ func TestKNodeGraphBuilder(t *testing.T) {
 	bu.Connect("fstore", "uvault")
 	bu.Connect("uvault", "wtf")
 
+	g, err := bu.BuildDGraph("test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p, err := DotGen(g, DotGenParam{Format: "png"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := sys.TermOpenUrl(p.GeneratedFile); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAsyncKNodeGraphBuilder(t *testing.T) {
+	bu := NewKNodeGraphBuilder()
+
+	var wg sync.WaitGroup
+	async := func(f func()) {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			f()
+		}()
+	}
+
+	async(func() { bu.Add("fstore", Node{Label: "fstore"}) })
+	async(func() { bu.Add("vfm", Node{Label: "vfm"}) })
+	async(func() { bu.Add("uvault", Node{Label: "user-vault"}) })
+
+	async(func() { bu.Connect("fstore", "vfm") })
+	async(func() { bu.Connect("uvault", "vfm") })
+	async(func() { bu.Connect("fstore", "uvault") })
+	async(func() { bu.Connect("uvault", "wtf") })
+
+	wg.Wait()
 	g, err := bu.BuildDGraph("test")
 	if err != nil {
 		t.Fatal(err)
